59f63c5d3425c9e2e4c28ded7623bd20c77bc947
DanielOses <dfo_utn@hotmail.com>
2022-01-23 15:15:38 -0300

    v 0.1

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,12 +1,39 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, UUID } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    release: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    platforms: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdInDB: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    }
   });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,8 @@
 const { Router } = require('express');
+const {Videogame, Genre} = require("../db.js");
+const { API_KEY } = process.env;
+const axios = require("axios");
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -7,6 +11,63 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+
+
+const getVideogames = async () => {
+
+    const api = await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}`)
+    const apiInfo = await api.data.results.map(el => {
+        return {
+            name: el.name,
+            image: el.background_image,
+            release: el.released,
+            rating: el.rating,
+            platforms: el.platforms.map(ch => ch.platform.name),
+            genres: el.genres.map(ch => ch.name)
+        };
+    });
+    return apiInfo;
+
+}
+
+const getDbInfo = async () => {
+    return await Videogame.findAll({include: Genre})
+}
+
+const getAllVideogames = async () => {
+    let apiInfo = await getVideogames();
+    let dbInfo = await getDbInfo();
+    let infoTotal = await apiInfo.concat(dbInfo);
+    return infoTotal;
+}
+
+ /*const db = await Videogame.findAll({include: Genre})
+        if (api) {
+            let apiResponse = api.data.results?.map((ch) => {
+                return {
+                    name: ch.name,
+                    image: ch.background_image,
+                    release: ch.released,
+                    rating: ch.rating,
+                }
+            })
+            let response = apiResponse;
+            res.send(api)
+        }*/
+router.get('/videogames', async(req, res) => {
+    const name = req.query.name;
+    let gamesTotal = await getAllVideogames();
+    if (name) {
+        let gameName = await gamesTotal.filter(el => el.name.toLowerCase().includes(name.toLowerCase()))
+        //toLowerCase();
+        gameName.length ? 
+        res.status(200).send(gameName) :
+        res.status(400).send('NONONONONONONO MALOOOOOO');
+    } else {
+        res.status(200).send(gamesTotal);
+    }
+})
 
 
 module.exports = router;
